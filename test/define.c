58469179434cb9fa554e8da6c3a54ce6577cb5b0
LuisHsu <luis8404272@gmail.com>
2016-06-11 04:27:54 +0800

    Add define test and re-orginize tests

---
+++ test/define.c
@@ -0,0 +1,7 @@
+int idDefine1;
+int idDefine2[2];
+int idFunction1(){}
+int idFunction2(int idParam1){}
+int idFunction3(int idParam1, int idParam2){}
+int idFunction4(int idParam1, int idParam2[]){}
+int idFunction5(int idParam1[], int idParam2[]){}
